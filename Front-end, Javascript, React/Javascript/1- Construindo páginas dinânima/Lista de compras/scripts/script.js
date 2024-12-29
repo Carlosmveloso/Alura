f4cd0be7297e4d82f30c4f0e4b7cadd0fdae7b19
Carlos Eduardo <carloseduardomveloso@gmail.com>
2024-12-29 20:55:27 -0300

    continuando os estudos de javascript

--- Front-end, Javascript, React/Javascript/1- Construindo páginas dinânima/Lista de compras/scripts/script.js
+++ Front-end, Javascript, React/Javascript/1- Construindo páginas dinânima/Lista de compras/scripts/script.js
@@ -1,10 +1,47 @@
 const item = document.getElementById("input-item");
 const botaosalvaritem = document.getElementById("adicionar-item");
+const ListaDeCompras = document.getElementById("lista-de-compras");
 
 botaosalvaritem.addEventListener("click", adicionarItem);//Ouvinte de eventos/ ("evento", função)
 
 function adicionarItem(evento) {
     evento.preventDefault()
-    console.log("Entrei na função");
     
+    const itemDaLista = document.createElement("li");//Estamos atribuindo a variável itemDaLista a criação da tag li.
+    const containerItemLista = document.createElement("div");
+    containerItemLista.classList.add("item-lista-container")//Atribuindo uma classe para a div.
+
+    const containerNomeDoItem = document.createElement("div");
+    const nomeDoItem = document.createElement("p");
+    nomeDoItem.innerText= item.value;//Aqui estou declarando que o valor que for digitado no input vai ser salvo aqui.
+    containerNomeDoItem.appendChild(nomeDoItem);
+
+    const containerBotoes = document.createElement("div");
+
+    const botaoRemover = document.createElement("button");
+    botaoRemover.classList.add("botao-acao");
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("botao-acao");
+
+    const imagemRemover = document.createElement("img");
+    imagemRemover.src = "img/delete.svg";//Adicionando uma imagem pelo javascript.
+    imagemRemover.alt = "Remover";//Comentário da imagem.
+
+    botaoRemover.appendChild(imagemRemover);
+    containerBotoes.appendChild(botaoRemover);
+
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
+
+    botaoEditar.appendChild(imagemEditar);
+    containerBotoes.appendChild(botaoEditar);
+
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
+
+    ListaDeCompras.appendChild(itemDaLista);
+
+    itemDaLista.appendChild(containerItemLista);//Atribui que o containerItemLista seja uma tag filho da de itemLista.
 }
