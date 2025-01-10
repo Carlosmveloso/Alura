4b54a9a146d83248c0bfd6a6d73ba6b687457142
Carlos Eduardo <carloseduardomveloso@gmail.com>
2025-01-10 16:52:02 -0300

    continuando estudos

--- Front-end, Javascript, React/Javascript/2- Promises_async_await/página de upload/scripts.js
+++ Front-end, Javascript, React/Javascript/2- Promises_async_await/página de upload/scripts.js
@@ -4,3 +4,60 @@ const inputUpload = document.getElementById("image-upload")
 uploadBtn.addEventListener("click", () => {
     inputUpload.click();
 })
+
+//Implementação de upload de arquivos
+//Funcionalidade de upload de arquivos
+
+function lerConteudoDoArquivo (arquivo) {
+    //você está dizendo que está fazendo uma operação que pode levar algum tempo para ser concluída, usando o promise.
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();// O FileReader é resposável por ler o arquivo
+        //Isso vai retorna quase tenha dado certo a leitura do aquivo .
+        leitor.onload = () => {
+            resolve({url: leitor.result, nome: arquivo.name});
+        }
+        //Vai retornar caso tenha dado errado a leitura do aquivo
+        leitor.onerror = () => {
+            reject(`Erro na leitura do arquivo ${arquivo.name}`);
+        }
+
+        leitor.readAsDataURL(arquivo)
+    })
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p")
+
+//Como lidar com funções assíncronas
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0];//Vai pergar o arquivo que foi enviado
+
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url;
+            nomeDaImagem.textContent = conteudoDoArquivo.nome;
+        } catch(error) {
+            console.error("Erro na leitura do arquivo");
+            
+        }
+    }
+})
+
+//Implementar a funcionalidade de inserção de tags da aplicação.
+
+const inputTags = document.getElementById("categoria");
+const listaTags = document.getElementById("lista-tags");
+//O keypress vao monitorar as teclas do teclado
+inputTags.addEventListener("keypress", (evento) => {
+    if(evento.key === "Enter") {
+    evento.preventDefault();
+    const tagTexto = inputTags.value.trim();
+    if (tagTexto != "") {
+        const tagNova = document.createElement("li");
+        tagNova.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`
+        listaTags.appendChild(tagNova);//Estou dizendo que a listaTags é filha de tagNova.
+        inputTags.value = "";
+    }
+  } 
+})
