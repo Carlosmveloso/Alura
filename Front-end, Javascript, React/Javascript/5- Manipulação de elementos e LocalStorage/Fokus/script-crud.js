dd7e6d926e29482b596dd1f7266f06e5da1e7885
Carlos Eduardo <carloseduardomveloso@gmail.com>
2025-02-18 20:23:43 -0300

    criando a local storage

--- Front-end, Javascript, React/Javascript/5- Manipulação de elementos e LocalStorage/Fokus/script-crud.js
+++ Front-end, Javascript, React/Javascript/5- Manipulação de elementos e LocalStorage/Fokus/script-crud.js
@@ -2,8 +2,20 @@
 
 const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+const textarea = document.querySelector(".app__form-textarea");
 
+const tarefas = [];
 
 btnAdicionarTarefa.addEventListener("click", () => {
-  formAdicionarTarefa.classList.toggle("hidden");//O método toggle faz a altenância na class hidden.
+  formAdicionarTarefa.classList.toggle("hidden"); //O método toggle faz a altenância na class hidden.
+});
+
+formAdicionarTarefa.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  //Criando um objeto
+  const tarefa = {
+    descricao: textarea.value,
+  };
+  tarefas.push(tarefa);//Mandando colocar a tarefa dentro da lista tarefas.
+  localStorage.setItem("tarefas", tarefas)
 });
