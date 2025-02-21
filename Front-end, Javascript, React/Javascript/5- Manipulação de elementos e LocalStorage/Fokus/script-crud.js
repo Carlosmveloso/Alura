a33b1a4dfa8fbf8395de6f44d0b170fcbf4d503b
Carlos Eduardo <carloseduardomveloso@gmail.com>
2025-02-21 14:28:49 -0300

    continuando os estudos

--- Front-end, Javascript, React/Javascript/5- Manipulação de elementos e LocalStorage/Fokus/script-crud.js
+++ Front-end, Javascript, React/Javascript/5- Manipulação de elementos e LocalStorage/Fokus/script-crud.js
@@ -3,8 +3,40 @@
 const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
+const ultarefas = document.querySelector(".app__section-task-list");
 
-const tarefas = [];
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+//Função de adicionar uma tarefa
+
+function criarElementoTarefa(tarefa) {
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
+
+  const svg = document.createElement("svg");
+  svg.innerHTML = `
+  <svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    </svg>
+  `;
+  const paragrafo = document.createElement("p");
+  paragrafo.textContent = tarefa.descricao;
+
+  const botao = document.createElement("button");
+  const imagemBotao = document.createElement("img");
+  imagemBotao.setAttribute("src", "/imagens/edit.png");
+
+  botao.append(imagemBotao);
+
+  li.append(svg);
+  li.append(paragrafo);
+  li.append(botao);
+
+  return li;
+}
 
 btnAdicionarTarefa.addEventListener("click", () => {
   formAdicionarTarefa.classList.toggle("hidden"); //O método toggle faz a altenância na class hidden.
@@ -16,6 +48,11 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
   const tarefa = {
     descricao: textarea.value,
   };
-  tarefas.push(tarefa);//Mandando colocar a tarefa dentro da lista tarefas.
-  localStorage.setItem("tarefas", tarefas)
+  tarefas.push(tarefa); //Mandando colocar a tarefa dentro da lista tarefas.
+  localStorage.setItem("tarefas", JSON.stringify(tarefas)); // A api JSON com o método stringify faz a conversão para string.
+});
+
+tarefas.forEach((tarefa) => {
+  const elementoTarefa = criarElementoTarefa(tarefa);
+  ultarefas.append(elementoTarefa);
 });
