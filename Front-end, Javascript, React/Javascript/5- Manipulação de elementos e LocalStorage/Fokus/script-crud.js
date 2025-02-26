ef0b1a16c668aba88f204eb0c583566aa2fc1335
Carlos Eduardo <carloseduardomveloso@gmail.com>
2025-02-26 10:34:13 -0300

    continuando com o projeto e os estudos

--- Front-end, Javascript, React/Javascript/5- Manipulação de elementos e LocalStorage/Fokus/script-crud.js
+++ Front-end, Javascript, React/Javascript/5- Manipulação de elementos e LocalStorage/Fokus/script-crud.js
@@ -3,9 +3,13 @@
 const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
-const ultarefas = document.querySelector(".app__section-task-list");
+const ulTarefas = document.querySelector(".app__section-task-list");
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+function atualizarTarefas() {
+  localStorage.setItem("tarefas", JSON.stringify(tarefas)); // A api JSON com o método stringify faz a conversão para string.
+}
 
 //Função de adicionar uma tarefa
 
@@ -15,17 +19,32 @@ function criarElementoTarefa(tarefa) {
 
   const svg = document.createElement("svg");
   svg.innerHTML = `
-  <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
   `;
   const paragrafo = document.createElement("p");
   paragrafo.textContent = tarefa.descricao;
+  paragrafo.classList.add("app__section-task-list-item-description");
 
   const botao = document.createElement("button");
+  botao.classList.add("app_button-edit");
+
+  // O onlick sobreescreve todos os outros apenas mostrando o ultimo a ser executado, enquanto o addEventListener acumula e mostra todos no final.
+  // Editando uma tarefa ja existente
+  botao.onclick = () => {
+    debugger
+    const novaDescricao = prompt("Qual o novo nome da tarefa ?");
+    console.log(`Nova descrição da tarefa: ${novaDescricao}`);
+
+    if (novaDescricao) {
+      paragrafo.textContent = novaDescricao;
+      tarefa.descricao = novaDescricao;
+      atualizarTarefas();
+    }
+  };
+
   const imagemBotao = document.createElement("img");
   imagemBotao.setAttribute("src", "/imagens/edit.png");
 
@@ -49,10 +68,16 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
     descricao: textarea.value,
   };
   tarefas.push(tarefa); //Mandando colocar a tarefa dentro da lista tarefas.
-  localStorage.setItem("tarefas", JSON.stringify(tarefas)); // A api JSON com o método stringify faz a conversão para string.
+  // Já atualizar as tarefas quando criado outra
+  const elementoTarefa = criarElementoTarefa(tarefa);
+  ulTarefas.append(elementoTarefa);
+  atualizarTarefas();
+  // Esvaziar o formulário e esconder quando enviado
+  textarea.value = "";
+  formAdicionarTarefa.classList.add("hidden");
 });
 
 tarefas.forEach((tarefa) => {
   const elementoTarefa = criarElementoTarefa(tarefa);
-  ultarefas.append(elementoTarefa);
+  ulTarefas.append(elementoTarefa);
 });
