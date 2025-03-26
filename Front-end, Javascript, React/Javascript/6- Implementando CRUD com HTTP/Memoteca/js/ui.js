0212c24c046b9688fa51e4f0281d5eb4fe297ce1
Carlos Eduardo <carloseduardomveloso@gmail.com>
2025-03-26 18:14:24 -0300

    continuando com as aulas e o projeto

--- Front-end, Javascript, React/Javascript/6- Implementando CRUD com HTTP/Memoteca/js/ui.js
+++ Front-end, Javascript, React/Javascript/6- Implementando CRUD com HTTP/Memoteca/js/ui.js
@@ -7,18 +7,41 @@ const interfaceDoUsuario = {
     const listaDePensamentos = document.getElementById("lista-pensamentos");
     try {
       const pensamentos = await requisiçõesApi.buscarPensamentos();
-      pensamentos.forEach((pensamento) => {
-        listaDePensamentos.innerHTML += `
-        <li class="li-pensamento" data-id="${pensamento.id}">
-            <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
-            <div class="pensamento-conteudo">${pensamento.conteudo}</div>
-            <div class="pensamento-autoria">${pensamento.autoria}</div>
-        </li>`;
-      });
+      pensamentos.forEach(interfaceDoUsuario.adicionarNovoPensamentoNaLista);
     } catch {
       alert("Erro ao renderizar o pensamento");
     }
   },
-};
 
+  //Criando a interface quando criado o novo pensamento
+  adicionarNovoPensamentoNaLista(pensamento) {
+    const listaDePensamentos = document.getElementById("lista-pensamentos");
+    const novoPensamento = document.createElement("li");
+    novoPensamento.setAttribute("data-id", pensamento.id);
+    novoPensamento.classList.add("li-pensamento");
+
+    //Adicionando a imagem de aspas dentro do card
+    const imagemAspas = document.createElement("img");
+    imagemAspas.src = "assets/imagens/aspas-azuis.png";
+    imagemAspas.alt = "Aspas azuis";
+    imagemAspas.classList.add("icone-aspas");
+
+    //Adicionando o conteúdo dentro do card
+    const conteudoDoPensamento = document.createElement("div");
+    conteudoDoPensamento.textContent = pensamento.conteudo;
+    conteudoDoPensamento.classList.add("pensamento-conteudo");
+
+    //Adicionando a autoria dentro do card
+    const autoriaDoPensamento = document.createElement("div");
+    autoriaDoPensamento.textContent = pensamento.autoria;
+    autoriaDoPensamento.classList.add("pensamento-autoria");
+
+    //Organizando os elementos dentro do card
+    novoPensamento.appendChild(imagemAspas);
+    novoPensamento.appendChild(conteudoDoPensamento);
+    novoPensamento.appendChild(autoriaDoPensamento);
+    listaDePensamentos.appendChild(novoPensamento);
+
+  },
+};
 export default interfaceDoUsuario;
