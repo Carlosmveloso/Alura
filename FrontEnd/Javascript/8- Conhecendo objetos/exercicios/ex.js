5ac4e095ac2f27f6fe9d3799ac8be24b788193d0
Carlos Eduardo <carloseduardomveloso@gmail.com>
2025-07-27 18:21:10 -0300

    continuando os exercicios

--- FrontEnd/Javascript/8- Conhecendo objetos/exercicios/ex.js
+++ FrontEnd/Javascript/8- Conhecendo objetos/exercicios/ex.js
@@ -1,10 +1,35 @@
+anoAtual = new Date().getFullYear();
+
 const livro = {
   titulo: "Nome do Livro",
   autor: "Autor do Livro",
   anoDePublicacao: 2000,
   genero: "Qualquer gênero",
-  idade: 5,
+  idade: anoAtual - 2000,
+  avaliacao: null,
 };
+
 console.log(livro);
-livro.avaliacao = ''
-console.log(livro);
+
+
+const novaAvaliacao = {
+  nota: 4.5,
+  comentario: 'Esse livro tocou meu coração !',
+}
+
+if (livro.avaliacao === null) {
+  livro.avaliacao = novaAvaliacao;
+} else {
+  console.log('Esse livro já tem uma avaliacão.');
+}
+
+console.log('Detalhes do livro: ')
+console.log(`Título: ${livro.titulo}`)
+console.log(`Autor: ${livro.autor}`)
+console.log(`Ano de Publicação: ${livro.anoDePublicacao}`)
+console.log(`Gênero: ${livro.genero}`)
+console.log(`Avaliação: ${livro.avaliacao === null 
+  ? 'Nenhuma avaliação disponível.' 
+  : `Nota: ${livro.avaliacao.nota}, Comentário: "${livro.avaliacao.comentario}"`}`);
+
+
